b4189298b2f09eaecae4cd0610d9c104bcde8616
Abstinence6 <40562781+Abstinence6@users.noreply.github.com>
2023-12-21 18:03:36 +0200

    1

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include "time.h"
 
 #define MFR_RST_PIN D3 // Configurable, see typical pin layout above 18
 #define MFR_SS_PIN D0  // Configurable, see typical pin layout above  16
@@ -28,6 +29,8 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP,"europe.pool.ntp.org",utc_in_sec);
 MFRC522 mfrc(MFR_SS_PIN, MFR_RST_PIN); // Create MFRC522 instance
 DS3231 myRTC;
+
+bool century = false, h12Flag, pmFlag;
 
 void setup()
 {
@@ -48,27 +51,25 @@ void setup()
   if (!SD.begin(0))
     tone(BUZZER, 300, 500); // Карта памяти не обнаружена или повреждена :(");
   else
-    Serial.println("SD Init");
+    Serial.print("\nSD Init :");
 
   File myFile = SD.open("test.txt", FILE_WRITE);
   if (myFile)
   {
-    Serial.print("Writing to test.txt...");
+    Serial.println(" good");
     myFile.close();
   }
   else
-  {
-    Serial.println("error opening test.txt");
-  }
+    Serial.println(" bad");
 
   digitalWrite(SD_SS_PIN, OFF);
   digitalWrite(MFR_SS_PIN, ON);
   delay(100);
 
+  Serial.println("MFRC Init");
   mfrc.PCD_Init();                // Init MFRC522
   mfrc.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
   mfrc.PCD_AntennaOn();
-  Serial.println("MFRC Init");
 
   digitalWrite(SD_SS_PIN, ON);
   digitalWrite(MFR_SS_PIN, OFF);
@@ -77,6 +78,7 @@ void setup()
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
+  Serial.print("\nWiFi connect");
   int ConnectTime = 0;
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -96,15 +98,33 @@ void setup()
 
   timeClient.begin();
   timeClient.update();
+
   // myRTC.setEpoch(time_t epoch = 0, bool flag_localtime = false);
-  myRTC.setSecond(timeClient.getSeconds());
-  myRTC.setMinute(timeClient.getMinutes());
-  myRTC.setHour(timeClient.getHours());
-  // myRTC.setDoW(byte DoW);
-  myRTC.setDate(timeClient.getDay());
-  myRTC.setMonth(12);
-  myRTC.setYear(23);
+
+  unsigned long long now = timeClient.getEpochTime();
+  struct tm *ptm = gmtime ((time_t *)&now);
+
+  myRTC.setSecond(ptm->tm_sec);
+  myRTC.setMinute(ptm->tm_min);
+  myRTC.setHour(ptm->tm_hour);
+  //myRTC.setDoW(timeClient.getDay());
+  myRTC.setDate(ptm->tm_mday);
+  myRTC.setMonth(ptm->tm_mon);
+  myRTC.setYear(ptm->tm_year-100);
   myRTC.setClockMode(0);
+
+  Serial.print(ptm->tm_year + 1900, DEC);
+  Serial.print("-");
+  Serial.print(ptm->tm_mon, DEC);
+  Serial.print("-");
+  Serial.print(ptm->tm_mday, DEC);
+  Serial.print(" ");
+  Serial.print(ptm->tm_hour, DEC); // 24-hr
+  Serial.print(":");
+  Serial.print(ptm->tm_min, DEC);
+  Serial.print(":");
+  Serial.println(ptm->tm_sec, DEC);
+
   Serial.println("Time init");
 
   Serial.println("Init done");
@@ -119,8 +139,6 @@ void dump_byte_array(byte *buffer, byte bufferSize)
   }
 }
 
-bool century = false, h12Flag, pmFlag;
-
 void loop()
 {
   ArduinoOTA.handle();
@@ -132,91 +150,5 @@ void loop()
     Serial.println();
   }
 
-  Serial.print(myRTC.getYear(), DEC);
-  Serial.print("-");
-  Serial.print(myRTC.getMonth(century), DEC);
-  Serial.print("-");
-  Serial.print(myRTC.getDate(), DEC);
-  Serial.print(" ");
-  Serial.print(myRTC.getHour(h12Flag, pmFlag), DEC); // 24-hr
-  Serial.print(":");
-  Serial.print(myRTC.getMinute(), DEC);
-  Serial.print(":");
-  Serial.println(myRTC.getSecond(), DEC);
 
-  delay(900);
 }
-/*
-String str_buf;
-String actual_date; 
-String actual_time;
-
-void check_date()   
-{
-  timeClient.update();
-  str_buf = timeClient.getFormattedDate(); // возвращается в таком виде 2021-11-30T16:00:13Z
-  int splitT = str_buf.indexOf("T");
-  actual_date = str_buf.substring(0,splitT); //2021-11-30
-  actual_date.replace("-",";");
-  str_buf = "";
-}
-//=======================================заполняет актуальное время используя данные из интернета======================================
-void check_time()        
-{
-   timeClient.update();
-  str_buf = timeClient.getFormattedDate(); // возвращается в таком виде 2021-11-30T16:00:13Z
-  int splitT = str_buf.indexOf("T");
-  actual_time = str_buf.substring(splitT+1,str_buf.length()-4); //16:00:13
-  str_buf = "";
-}
-*/
-/*
-void setup()
-{
-  Wire.begin();
-
-  Serial.begin(9600);
-  Serial.println("\nI2C Scanner");
-}
-
-
-void loop()
-{
-  byte error, address;
-  int nDevices;
-
-  Serial.println("Scanning...");
-
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
-
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknow error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
-
-  delay(5000);           // wait 5 seconds for next scan
-}
-*/
